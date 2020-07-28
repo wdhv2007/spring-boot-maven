b69bd9d96606add73488d07aa196ee4c8f327dfb
woo dongha <josh@shapl.com>
2020-07-28 11:47:00 +0900

    최초커밋

---
+++ src/main/java/com/example/mtest/MtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtestApplication.class, args);
+	}
+
+}
